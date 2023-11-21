d26ed584e57f9162fc920a08d3029612f6f22b5e
YOUNG KIRK <adjanour@icloud.com>
2023-11-21 18:13:01 +0000

    Add project files.

---
+++ Northwind.Common.EntityModels.Sqlite/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared;
+
+[Index("CategoryId", Name = "CategoriesProducts")]
+[Index("CategoryId", Name = "CategoryId")]
+[Index("ProductName", Name = "ProductName")]
+[Index("SupplierId", Name = "SupplierId")]
+[Index("SupplierId", Name = "SuppliersProducts")]
+public partial class Product
+{
+    [Key]
+    public long ProductId { get; set; }
+
+    [Column(TypeName = "nvarchar (40)")]
+    [StringLength(40)]
+    public string ProductName { get; set; } = null!;
+
+    [Column(TypeName = "INT")]
+    public long? SupplierId { get; set; }
+
+    [Column(TypeName = "INT")]
+    public long? CategoryId { get; set; }
+
+    [Column(TypeName = "nvarchar (20)")]
+    [StringLength(20)]
+    public string? QuantityPerUnit { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal? UnitPrice { get; set; }
+
+    [Column(TypeName = "smallint")]
+    public long? UnitsInStock { get; set; }
+
+    [Column(TypeName = "smallint")]
+    public long? UnitsOnOrder { get; set; }
+
+    [Column(TypeName = "smallint")]
+    public long? ReorderLevel { get; set; }
+
+    [Column(TypeName = "bit")]
+    public bool Discontinued { get; set; } 
+
+    [ForeignKey("CategoryId")]
+    [InverseProperty("Products")]
+    public virtual Category? Category { get; set; }
+
+    [InverseProperty("Product")]
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+    [ForeignKey("SupplierId")]
+    [InverseProperty("Products")]
+    public virtual Supplier? Supplier { get; set; }
+}
