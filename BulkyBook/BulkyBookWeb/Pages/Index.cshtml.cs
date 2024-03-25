e47c561df802f2fe209d3c8503787859418a6af3
Adjanour <adjanour@icloud.com>
2024-03-25 20:48:23 +0000

    Update launchSettings.json and index.cshtml

---
+++ BulkyBook/BulkyBookWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace BulkyBookWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public string? DayName { get; set; }
+        public void OnGet()
+        {
+            DayName = DateTime.Now.ToString("dddd");
+        }
+    }
+}
